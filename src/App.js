366bf180e4244da860819bb686f18ba005bb037b
arshcherbakov <shcherbakov.fortech@gmail.com>
2024-06-11 13:15:43 +0300

    version 2 add isLoading

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import "./App.css";
 const useQuery = ({ argsParams, queryFn }) => {
   const [data, setData] = useState([]);
   const [isPending, setIsPending] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); // можно сделать бачинг isPending, isLoading, isError
   const [isError, setIsError] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(() => {
+    setIsLoading(true);
+  }, []);
+
+  useEffect(() => {
     setIsPending(true);
+    setIsLoading(false);
 
     const getData = async () => {
       try {
         const response = await queryFn();
         const data = await response.json();
+
         setData(data.results);
         setIsPending(false);
       } catch (error) {
         setIsError(true);
         setError(error);
+        setIsPending(false);
       }
     };
 
@@ -42,6 +49,7 @@ const App = () => {
       fetch(" https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"),
   });
 
+  console.log(isLoading);
   return (
     <div className="App">
       {isPending ? (
